1bd8fda697193e20454ddaa5bc89fe1f60a5bb99
RodrigoSierraV <735@holbertonschool.com>
2019-08-13 05:40:15 +0000

    Task 5, new public instance method: def delete(self, obj=None) and Update the prototype of def all(self) to def all(self, cls=None) - that returns the list of objects of one type of class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,11 +20,14 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """returns a dictionary
         Return:
             returns a dictionary of __object
         """
+        if cls:
+            return {key: value for key, value in self.__objects.items()
+                    if value.__class__ == cls}
         return self.__objects
 
     def new(self, obj):
@@ -56,3 +59,10 @@ class FileStorage:
                     self.__objects[key] = value
         except:
             pass
+
+    def delete(self, obj=None):
+        """delete obj from __objects if its inside"""
+
+        if obj:
+            del self.__objects[obj.__class__.__name__ + '.' + obj.id]
+            self.save()
