7889c30b6ebbbe7384eb7a5c523e4f71ea91d01f
root <hvelez020@gmail.com>
2019-08-13 23:44:21 +0000

    [add] db feature to STATES

--- models/state.py
+++ models/state.py
@@ -1,11 +1,27 @@
 #!/usr/bin/python3
 """This is the state class"""
-from models.base_model import BaseModel
+from os import environ
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+import models
 
-
-class State(BaseModel):
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    if environ['HBNB_TYPE_STORAGE'] == 'db':
+        cities = relationship('City', backref="state")
+    else:
+        @property
+        def cities(self):
+            """ getter for cities """
+
+            tmp_list = []
+            for key, obj in models.storage.items():
+                if "City" in key and obj.state_id == self.id:
+                    tmp_list.append(obj)
+            return tmp_list
