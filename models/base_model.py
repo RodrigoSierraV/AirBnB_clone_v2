a8c5e9f85430ecaf67e8eb06a820a8f3731e963b
root <hvelez020@gmail.com>
2019-08-13 20:24:38 +0000

    [update] base_model new method, delete and only save to __objects when save is called

--- models/base_model.py
+++ models/base_model.py
@@ -39,8 +39,8 @@ class BaseModel:
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
-            models.storage.new(self)
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """returns a string
@@ -59,6 +59,7 @@ class BaseModel:
         """updates the public instance attribute updated_at to current
         """
         self.updated_at = datetime.now()
+        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
@@ -70,4 +71,16 @@ class BaseModel:
         my_dict["__class__"] = str(type(self).__name__)
         my_dict["created_at"] = self.created_at.isoformat()
         my_dict["updated_at"] = self.updated_at.isoformat()
+        if "_sa_instance_state" in my_dict:
+            del my_dict["_sa_instance_state"]
         return my_dict
+
+    def delete(self):
+        """delete the current instance from the storage (models.storage)
+        by calling the method delete
+
+        Return:
+            none
+        """
+        models.storage.delete(self)
+
