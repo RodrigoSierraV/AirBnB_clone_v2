bdc1c6527ec8c3d6fb666d447e5f43a9c1a69a51
RodrigoSierraV <735@holbertonschool.com>
2019-08-13 23:59:05 +0000

    Importing Base to City

--- models/city.py
+++ models/city.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This is the city class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import String
 from sqlalchemy import ForeignKey
 
