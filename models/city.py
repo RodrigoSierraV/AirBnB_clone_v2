6d455b0697c47ae79caa74e98feca9d7ba62b44b
RodrigoSierraV <735@holbertonschool.com>
2019-08-14 16:26:31 +0000

    Replacing attributes in classes User and Place to work with sqlalchemy

--- models/city.py
+++ models/city.py
@@ -3,7 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import String, Column
 from sqlalchemy import ForeignKey
-
+from sqlalchemy.orm import relationship
 
 class City(BaseModel, Base):
     """This is the class for City
@@ -11,6 +11,8 @@ class City(BaseModel, Base):
         state_id: The state id
         name: input name
     """
+
     __tablename__ = 'cities'
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
+    places = relationship('Place', backref='cities')
--- models/place.py
+++ models/place.py
@@ -18,14 +18,16 @@ class Place(BaseModel):
         longitude: longitude in float
         amenity_ids: list of Amenity ids
     """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
+
+    __tablename__ = 'places'
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = (String(1024), nullable=True)
+    number_rooms = Column(Integer, default=0, nullable=False)
+    number_bathrooms = Column(Integer, default=0, nullable=False)
+    max_guest = Column(Integer, default=0, nullable=False)
+    price_by_night = Column(Integer, default=0, nullable=False)
+    latitude = Column(Float, nullable=Tue)
+    longitude = Column(Float, nullable=Tue)
     amenity_ids = []
