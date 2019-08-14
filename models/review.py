597ba3611053ec69c68dc29ca8d98e651d9cc2fb
RodrigoSierraV <735@holbertonschool.com>
2019-08-14 20:03:45 +0000

    Importing Base

--- models/review.py
+++ models/review.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This is the review class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import String, Column
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
