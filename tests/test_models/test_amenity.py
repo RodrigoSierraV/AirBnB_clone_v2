d376c19493c714601d46bcae34091cc8feca41c2
RodrigoSierraV <735@holbertonschool.com>
2019-08-15 21:53:41 +0000

    Fix SkipIf

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -53,7 +53,9 @@ class TestAmenity(unittest.TestCase):
         """test attribute type for Amenity"""
         self.assertEqual(type(self.amenity.name), str)
 
-    @unittest.skipIf(os.getenv['HBNB_TYPE_STORAGE'] == 'db' )
+    @unittest.skipIf(os.getenv['HBNB_TYPE_STORAGE'] == 'db',
+                     'Not supported in Data Base mode')
+
     def test_save_Amenity(self):
         """test if the save works"""
         self.amenity.save()
--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -56,7 +56,9 @@ class TestCity(unittest.TestCase):
         self.assertEqual(type(self.city.name), str)
         self.assertEqual(type(self.city.state_id), str)
 
-    @unittest.skipIf(os.getenv['HBNB_TYPE_STORAGE'] == 'db' )
+    @unittest.skipIf(os.getenv['HBNB_TYPE_STORAGE'] == 'db',
+                     'Not supported in Data Base mode')
+
     def test_save_City(self):
         """test if the save works"""
         self.city.save()
