519c5be40f9e19e388d53c8123e20ca75e599b9a
root <hvelez020@gmail.com>
2019-08-15 01:21:47 +0000

    [fix] importing table in the file amenity

--- models/amenity.py
+++ models/amenity.py
@@ -4,7 +4,7 @@ from os import environ
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-
+from models.place import place_amenity
 
 class Amenity(BaseModel, Base):
     """This is the class for Amenity
