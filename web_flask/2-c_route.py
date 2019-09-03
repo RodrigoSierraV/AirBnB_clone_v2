42e62db4f3ccb8b76a8328c46b8d32275a6719d4
RodrigoSierraV <735@holbertonschool.com>
2019-09-03 22:58:14 +0000

    [Fix] pep8

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -4,13 +4,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def home():
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     return 'HBNB'
+
 
 @app.route("/c/<text>", strict_slashes=False)
 def hbnb_text(text):
