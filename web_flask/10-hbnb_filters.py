7ad7a6b069e787c950f52e2c1582dde06a1fd666
RodrigoSierraV <735@holbertonschool.com>
2019-09-05 05:04:50 +0000

    [Add] feature filters

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application /states_list: display a HTML
+page: (inside the tag BODY)
+"""
+
+from flask import Flask, render_template
+import models
+
+app = Flask(__name__)
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def filters():
+    """lists all State objects present in DBStorage"""
+
+    states = models.storage.all('State').values()
+    amenities = models.storage.all('Amenity').values()
+    return render_template('10-hbnb_filters.html', states=states,
+                           amenities=amenities)
+
+
+@app.teardown_appcontext
+def teardb(statesList):
+    """ Closes dbstorage"""
+
+    models.storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
